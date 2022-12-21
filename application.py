448e84a8604b3d9b449388a4c2c00ebbdf320d64
Lijie Huang <huanglj666@outlook.com>
2022-12-20 20:11:25 -0500

    fix url

--- application.py
+++ application.py
@@ -6,7 +6,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask import request
 
-search_url = "http://Cloudsearch-env.eba-gr976pa4.us-east-1.elasticbeanstalk.com"
+search_url = "http://Cloudsearch-env.eba-gr976pa4.us-east-1.elasticbeanstalk.com/elasticsearch"
 topic_arn = "arn:aws:sns:us-east-1:964216032660:6156_to_lambda"
 
 pymysql.install_as_MySQLdb()
